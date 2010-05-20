6256cf73bbd9e62e008a80a2a3b1b08fc80d277d
gmallard <allard.guy.m@gmail.com>
2010-05-20 07:48:43 -0400

    Initial commit to gmallard_test_hw01.

---
+++ test/helper.rb
@@ -0,0 +1,9 @@
+require 'rubygems'
+require 'test/unit'
+
+$LOAD_PATH.unshift(File.dirname(__FILE__))
+$LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
+require 'gmallard_test_hw01'
+
+class Test::Unit::TestCase
+end
